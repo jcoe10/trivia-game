687c9d9d20f1583c8d5d8ab44345feadd6e11409
jcoe10 <jacob.e.coe@gmail.com>
2017-08-31 09:07:35 -0400

    added index assets and readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is the easy version using jQuery, HTML, and CSS.
 
+This is a trivia game made in jQuery about Classic Rock.
+Using radio buttons to answer each question. Enjoy! :)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,160 @@
-$(document).ready(function() {
-    var currentQuestion = 0;
-    var $question = $('.question');
-    var totalQuestions = $('.question').length;
-    var correct = 0;
-    var wrong = 0;
-    var score = 0;
-    $question.hide();
+$(document).ready(function () {
+    $('#quiz, #previous, #next, #submit, #results, #questions').hide();
+    var $start = $('<button type="button" class="start" value="start">Start</button>');
+    $('#start').append($start);
+    $($start).on('click', function () {
+        $('#quiz, #previous, #next, #submit, #results, #questions').show();
+        $('#start').hide();
 
-    $(".start").on( "click", function() {
+    })
+    setTimeout(function () {
+        $('#quiz, #previous, #submit, #results, #questions').hide();
+        $('#next, #quiz').show();
+    }, 1000 * 15);
 
+    const myQuestions = [{
+            question: "Who sings Walk this Way?",
+            answers: {
+                a: "Aerosmith",
+                b: "AC/DC",
+                c: "Guns N' Roses",
+                d: "Poison"
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "Which band sings the famous song Back in Black?",
+            answers: {
+                a: "Queen",
+                b: "Journey",
+                c: "AC/DC",
+                d: "Scorpions"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "What group sings Don't Stop Believin?",
+            answers: {
+                a: "Muse",
+                b: "Boston",
+                c: "Iron Maiden",
+                d: "Journey"
+            },
+            correctAnswer: "d"
+        }
+    ];
 
-    
-    });
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // we'll want to store the list of answer choices
+            const answers = [];
+
+            // and for each available answer...
+            for (letter in currentQuestion.answers) {
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+               <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+             </label>`
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                `<div class="slide">
+             <div class="question"> ${currentQuestion.question} </div>
+             <div class="answers"> ${answers.join("")} </div>
+           </div>`
+            );
+        });
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join("");
+    }
+
+    function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll(".answers");
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = "lightgreen";
+            } else {
+                // if answer is wrong or blank
+                // color the answers red
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+
+        if (currentSlide === 0) {
+            previousButton.style.display = "none";
+        } else {
+            previousButton.style.display = "inline-block";
+        }
+
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = "none";
+            submitButton.style.display = "inline-block";
+        } else {
+            nextButton.style.display = "inline-block";
+            submitButton.style.display = "none";
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    const quizContainer = document.getElementById("quiz");
+    const resultsContainer = document.getElementById("results");
+    const submitButton = document.getElementById("submit");
+
+    // display quiz right away
+    buildQuiz();
+
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    showSlide(0);
+
+    // on submit, show results
+    submitButton.addEventListener("click", showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
 
 });
